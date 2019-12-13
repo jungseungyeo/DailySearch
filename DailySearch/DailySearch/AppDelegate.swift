dd4e1108605b5193b52b2ec285fb08580c757ebb
linsaeng <duwjdtmd91@gmail.com>
2019-12-13 21:59:11 +0900

    Create Project

---
+++ DailySearch/DailySearch/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  DailySearch
+//
+//  Created by saenglin on 2019/12/13.
+//  Copyright © 2019 linsaeng. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        guard #available(iOS 13, *) else {
+            // iOS 13 미만
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.backgroundColor = .clear
+            window?.rootViewController = IntroViewController()
+            window?.makeKeyAndVisible()
+            return true
+        }
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }}
+
